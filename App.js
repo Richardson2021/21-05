3528214fc36d16e980a112cdc531ac208db639f5
Richardson2021 <96339724+Richardson2021@users.noreply.github.com>
2022-05-21 10:19:40 +0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,40 @@
+import React from "react";
+import data from "./data.json";
+import Products from "./components/Products";
+
+
+
+
+class App extends React.Component {
+  constructor(){
+    super();
+    this.state = {
+      products: data.products,
+      size: "",
+      sort: "",
+    };
+  } 
+  render() {
+  
+
+  
+  return (
+    <div className="grid-container">
+      <header>
+        <a href="/">React Shopping cart</a>
+      </header>
+      <main>
+        <div className="content">
+          <div className="main">
+            <Products products={this.state.products}></Products>
+            </div>
+          <div className="sidebar">Cart Items</div>
+        </div>
+      </main>
+      <footer>All right</footer>
+    </div>
+  );
+  }
+}
+
+export default App;
